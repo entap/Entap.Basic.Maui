71159b689a0b93e70d41280c7e1ac0edb4a8bd5b
伊賀辰也 <iga@entap.co.jp>
2022-06-10 09:54:37 +0900

    [add]ObservableCollectionExtensions

---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Shared/Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reflection;
+
+namespace Entap.Basic.Maui.Core
+{
+    /// <summary>
+    /// ObservableCollectionの拡張メソッド
+    /// </summary>
+    public static class ObservableCollectionExtensions
+    {
+        #region AddRange
+        /// <summary>
+        /// ObservableCollection型のデータの末尾に、IEnumerable型のデータを追加する
+        /// </summary>
+        public static void AddRange<T>(this ObservableCollection<T> source, IEnumerable<T>? collection)
+        {
+            if (collection == null) return;
+            if (ValidateCollectionCount(source, collection))
+            {
+                return;
+            }
+
+            var property = GetItemsPropertyInfo<T>();
+            var method = GetOnCollectionResetMethodInfo<T>();
+
+            if (property?.GetValue(source) is List<T> list)
+            {
+                list.AddRange(collection);
+                method?.Invoke(source, null);
+            }
+        }
+
+        const int switchForeachThresold = 2;
+        static bool ValidateCollectionCount<T>(ObservableCollection<T> source, IEnumerable<T> collection)
+        {
+            var count = collection.Count();
+            if (count <= switchForeachThresold)
+            {
+                foreach (var item in collection)
+                {
+                    source.Add(item);
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        static PropertyInfo? GetItemsPropertyInfo<T>() => typeof(ObservableCollection<T>).GetProperty("Items", BindingFlags.NonPublic | BindingFlags.Instance);
+        static MethodInfo? GetOnCollectionResetMethodInfo<T>() => typeof(ObservableCollection<T>).GetMethod("OnCollectionReset", BindingFlags.NonPublic | BindingFlags.Instance);
+        #endregion
+    }
+}
