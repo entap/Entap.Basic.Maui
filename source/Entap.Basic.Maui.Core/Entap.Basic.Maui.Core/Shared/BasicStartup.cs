af7ae332b027a744b645a8789daa9121f424f2cb
伊賀辰也 <iga@entap.co.jp>
2022-06-13 15:42:46 +0900

    [add]BasicStartup

---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Shared/BasicStartup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Entap.Basic.Maui.Core
+{
+	/// <summary>
+    /// アプリケーションに登録した依存性を保有する
+    /// </summary>
+	public static class BasicStartup
+	{
+		static MauiApp? _mauiApp;
+		public static void Init(MauiApp mauiApp)
+		{
+			if (mauiApp is null)
+				throw new ArgumentNullException(nameof(mauiApp));
+
+			_mauiApp = mauiApp;
+		}
+
+		public static MauiApp MauiApp
+		{
+			get
+			{
+				if (_mauiApp is null)
+					throw new InvalidOperationException($"You MUST call {typeof(BasicStartup)}.{nameof(Init)}; prior to using it.");
+
+				return _mauiApp;
+			}
+		}
+	}
+}
+
