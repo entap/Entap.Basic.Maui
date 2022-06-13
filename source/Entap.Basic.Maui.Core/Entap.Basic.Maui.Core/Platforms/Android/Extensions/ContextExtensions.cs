fcf438605f92c7e313a611bbb35699bd50f38ede
伊賀辰也 <iga@entap.co.jp>
2022-06-13 09:48:37 +0900

    [add]ContextExtensions

---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Platforms/Android/Extensions/ContextExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.Versioning;
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace Entap.Basic.Maui.Core.Platform.Android
+{
+	/// <summary>
+    /// Context拡張機能
+    /// </summary>
+	public static class v
+	{
+        /// <summary>
+        /// ForegroundServiceを起動する
+        /// https://docs.microsoft.com/ja-jp/xamarin/android/app-fundamentals/services/foreground-services
+        /// </summary>
+        public static void StartForegroundServiceCompat<T>(this Context context, Bundle? args = null) where T : Service
+        {
+            var intent = new Intent(context, typeof(T));
+            if (args != null)
+            {
+                intent.PutExtras(args);
+            }
+
+            if (OperatingSystem.IsAndroidVersionAtLeast(26))
+            {
+                // Android 8.0(Oreo)以降
+                context.StartForegroundService(intent);
+            }
+            else
+            {
+                context.StartService(intent);
+            }
+        }
+
+        /// <summary>
+        /// Serviceを停止する
+        /// </summary>
+        public static bool StopService<T>(this Context context) where T : Service
+        {   
+            var intent = new Intent(context, typeof(T));
+            return context.StopService(intent);
+        }
+    }
+}
+
