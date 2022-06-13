7382bc488e132a901c0a71079c619ec61b120a64
伊賀辰也 <iga@entap.co.jp>
2022-06-13 14:10:31 +0900

    [add]Android : PlatformHandler

---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Platforms/Android/Modules/PlatformHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Android.App;
+
+namespace Entap.Basic.Maui.Core.Android
+{
+	/// <summary>
+    /// プラットフォーム固有のハンドリングを行う
+    /// </summary>
+	public static class PlatformHandler
+	{
+		public static void HandleAll(Activity activity)
+        {
+			AutofillHandler.DisableAutofill(activity);
+        }
+	}
+}
+
---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Platforms/Android/Platform.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Android.App;
+using Microsoft.Maui.Controls.Internals;
+
+namespace Entap.Basic.Maui.Core.Android
+{
+    [Preserve(AllMembers = true)]
+    public static class Platform
+    {
+        static Activity? _activity;
+        public static Activity Activity
+        {
+            get
+            {
+                if (_activity is null)
+                    throw new InvalidOperationException($"You MUST call {typeof(Platform)}.{nameof(Init)}; prior to using it.");
+                return _activity;
+            }
+            private set
+            {
+                _activity = value;
+            }
+        }
+
+        public static void Init(Activity activity, bool disablePlatformHandling = false)
+        {
+            if (activity is null)
+                throw new ArgumentNullException(nameof(activity));
+
+            Activity = activity;
+
+            if (!disablePlatformHandling)
+                PlatformHandler.HandleAll(activity);
+        }
+    }
+}
+
