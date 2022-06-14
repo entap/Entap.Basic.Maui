276b04645f2ba0103b83885bd28d6e29f56a3f02
伊賀辰也 <iga@entap.co.jp>
2022-06-14 10:26:40 +0900

    [add]Application

---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Shared/Application.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MauiApplication = Microsoft.Maui.Controls.Application;
+namespace Entap.Basic.Maui.Core
+{
+	/// <summary>
+    /// アプリケーション
+    /// </summary>
+	public class Application
+	{
+		public Application()
+		{
+		}
+
+		public static MauiApplication Current => MauiApplication.Current ?? throw new InvalidOperationException();
+	}
+}
+
