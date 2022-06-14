a7d87949cc2d754ba4c83e06fe1b547b675f20b6
伊賀辰也 <iga@entap.co.jp>
2022-06-14 10:55:00 +0900

    [update]MainPage : UI整理、ViewModel追加

---
+++ source/Entap.Basic.Maui.Core/Sample/Sample/UI/Pages/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Entap.Basic.Maui.Core;
+
+namespace Sample
+{
+	public class MainPageViewModel : PageViewModelBase
+	{
+		public MainPageViewModel()
+		{
+		}
+
+		public ProcessCommand PageManagerCommand => new(async () =>
+		{
+			await Task.CompletedTask;
+		});
+	}
+}
+
