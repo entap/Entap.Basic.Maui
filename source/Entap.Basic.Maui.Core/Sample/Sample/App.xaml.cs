06f84e90cdc5569ce90444050089185f163ad35b
伊賀辰也 <iga@entap.co.jp>
2022-06-14 10:53:43 +0900

    [update]Sample : App : MainPageをNavigationPageとして設定

--- source/Entap.Basic.Maui.Core/Sample/Sample/App.xaml.cs
+++ source/Entap.Basic.Maui.Core/Sample/Sample/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Sample;
+﻿using Entap.Basic.Maui.Core;
+
+namespace Sample;
 
 public partial class App : Application
 {
@@ -6,7 +8,7 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		PageManager.Navigation.SetNavigationMainPage<MainPage>();
 	}
 }
 
