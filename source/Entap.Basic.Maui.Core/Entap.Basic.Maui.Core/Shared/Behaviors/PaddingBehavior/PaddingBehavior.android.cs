a4f701487cdabaa26d5afa612a937940924a0ac0
Iga <iga@entap.co.jp>
2022-08-04 10:29:14 +0900

    [add]PaddingBehavior

---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Shared/Behaviors/PaddingBehavior/PaddingBehavior.android.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Entap.Basic.Maui.Core
+{
+    public partial class PaddingBehavior
+    {
+        global::Android.Views.View? _platformView;
+
+        protected override void OnAttachedTo(View bindable, global::Android.Views.View platformView)
+        {
+            base.OnAttachedTo(bindable, platformView);
+            _platformView = platformView;
+            PlatformUpdatePadding(Padding);
+        }
+
+        protected override void OnDetachedFrom(View bindable, global::Android.Views.View platformView)
+        {
+            base.OnDetachedFrom(bindable, platformView);
+        }
+
+        void PlatformUpdatePadding(Thickness padding)
+        {
+            if (_platformView is null) return;
+
+            var density = DisplaySizeManager.Density;
+            _platformView.SetPadding(
+                (int)(padding.Left * density),
+                (int)(padding.Top * density),
+                (int)(padding.Right * density),
+                (int)(padding.Bottom * density));
+        }
+    }
+}
+
---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Shared/Behaviors/PaddingBehavior/PaddingBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Entap.Basic.Maui.Core
+{
+    public partial class PaddingBehavior : PlatformBehavior<View>
+    {
+        public PaddingBehavior()
+        {
+        }
+
+        #region Padding BindableProperty
+        public static readonly BindableProperty PaddingProperty = BindableProperty.Create(
+            nameof(Padding),
+            typeof(Thickness),
+            typeof(PaddingBehavior),
+            null,
+            defaultBindingMode: BindingMode.Default,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                if (bindable is not PaddingBehavior behavior) return;
+                behavior.UpdatePadding();
+            });
+
+        public Thickness Padding
+        {
+            get { return (Thickness)GetValue(PaddingProperty); }
+            set { SetValue(PaddingProperty, value); }
+        }
+        #endregion
+
+        void UpdatePadding()
+        {
+#if ANDROID
+            PlatformUpdatePadding(Padding);
+#endif
+        }
+    }
+}
+
