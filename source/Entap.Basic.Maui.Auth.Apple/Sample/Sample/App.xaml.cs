f8e518775074151d15790008b836bbaa58f0ace9
Iga <iga@entap.co.jp>
2022-09-02 16:27:45 +0900

    [update]Entap.Basic.Maui.Auth.Apple : サンプルアプリでのAppleSignIn実装

--- source/Entap.Basic.Maui.Auth.Apple/Sample/Sample/App.xaml.cs
+++ source/Entap.Basic.Maui.Auth.Apple/Sample/Sample/App.xaml.cs
@@ -7,7 +7,16 @@ public partial class App : Application
 		InitializeComponent();
 		Entap.Basic.Maui.Auth.Apple.Core.Init();
 
-		MainPage = new AppShell();
+        if (OperatingSystem.IsIOS() ||
+            OperatingSystem.IsMacOS())
+        {
+            Entap.Basic.Maui.Auth.Apple.AppleSignInService.Init(
+                Entap.Basic.Maui.Auth.Apple.AuthorizationScope.Email,
+                Entap.Basic.Maui.Auth.Apple.AuthorizationScope.FullName
+            );
+        }
+
+        MainPage = new AppShell();
 	}
 }
 
--- source/Entap.Basic.Maui.Auth.Apple/Sample/Sample/MainPage.xaml.cs
+++ source/Entap.Basic.Maui.Auth.Apple/Sample/Sample/MainPage.xaml.cs
@@ -6,6 +6,27 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    void AppleSignInButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        if (OperatingSystem.IsIOS() ||
+            OperatingSystem.IsMacOS())
+            AppleSignInAsync().ConfigureAwait(false);
+    }
+
+    async Task AppleSignInAsync()
+	{
+        try
+        {
+            var service = new Entap.Basic.Maui.Auth.Apple.AppleSignInService();
+            var result = await service.SignInAsync();
+            await DisplayAlert("認証済しました。", $"{result.Email}{Environment.NewLine}{result.PersonName.FullName}", "OK");
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine(ex);
+        }
+    }
 }
 
 
