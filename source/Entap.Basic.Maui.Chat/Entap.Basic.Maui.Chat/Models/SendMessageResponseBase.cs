7351c61c74ca100a12670a9bb19a716f9800d3f8
Iga <iga@entap.co.jp>
2022-09-20 14:13:40 +0900

    [add]Entap.Basic.Maui.Chat : SendMessageResponseBase : Entap.Chatから移植

---
+++ source/Entap.Basic.Maui.Chat/Entap.Basic.Maui.Chat/Models/SendMessageResponseBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Entap.Basic.Maui.Chat
+{
+    [Preserve(AllMembers = true)]
+    public class SendMessageResponseBase
+    {
+        public int MessageId { get; set; }
+        public DateTime SendDateTime { get; set; }
+        public string? MediaThumbnailUrl { get; set; }
+    }
+}
+
