f6d2abc75e77e78d055aebdb87e1c108a0c4dad0
Iga <iga@entap.co.jp>
2022-09-20 14:12:09 +0900

    [update]Entap.Basic.Maui.Chat : ChatMemberBase : Entap.Chatから移植
    Nullable有効化に伴い、コンストラクタ定義

---
+++ source/Entap.Basic.Maui.Chat/Entap.Basic.Maui.Chat/Models/ChatMemberBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Entap.Basic.Maui.Chat
+{
+    [Preserve(AllMembers = true)]
+    public class ChatMemberBase
+	{
+        public ChatMemberBase(string userId, string userName)
+        {
+            UserId = userId;
+            UserName = userName;
+        }
+
+        public string UserId { get; set; }
+        public string? UserIcon { get; set; }
+        public string UserName { get; set; }
+        public int RoomAdmin { get; set; }
+    }
+}
+
