e31e83c02c4acbc73ce10ff66391fd54d128ad2f
Iga <iga@entap.co.jp>
2022-09-20 14:19:53 +0900

    [add]Entap.Basic.Maui.Chat : IChatControlService : Entap.Chatから移植

---
+++ source/Entap.Basic.Maui.Chat/Entap.Basic.Maui.Chat/Interfaces/IChatControlService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Entap.Basic.Maui.Chat
+{
+	public interface IChatControlService
+	{
+        Task<string> SelectImage();
+        Task<string> TakePicture();
+        Task ImageShare(string imagePath);
+        Task VideoShare(string videoPath);
+        void MoveImagePreviewPage(string imageUrl);
+        void MoveVideoPreviewPage(string imageUrl);
+        Task<IEnumerable<MessageBase>> BottomControllerMenuExecute(int notSendMessageId, int type, int roomId, ChatListView chatListView);
+    }
+}
+
