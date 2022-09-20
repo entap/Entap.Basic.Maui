6369812672241e7f315ddfc580b77374ba466fa3
Iga <iga@entap.co.jp>
2022-09-20 14:41:08 +0900

    [add]Entap.Basic.Maui.Chat : MessageType : Entap.Chatから移植

---
+++ source/Entap.Basic.Maui.Chat/Entap.Basic.Maui.Chat/Constants/MessageType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Entap.Basic.Maui.Chat
+{
+    /// <summary>
+    /// メッセージタイプ
+    /// </summary>
+    public enum MessageType
+    {
+        None,
+        Text,
+        Image,
+        Video,
+        MemberAddRoom = 10,
+        MemberLeaveRoom = 11,
+        Custom = 100
+    }
+}
