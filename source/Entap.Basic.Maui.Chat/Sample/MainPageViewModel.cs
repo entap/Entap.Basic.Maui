9fb18ee3e781511370eb2e18a37e6eb5ab2e76d2
Iga <iga@entap.co.jp>
2022-09-21 08:14:03 +0900

    [add]Entap.Basic.Maui.Chat : Sample : ChatPage追加、ページ遷移実装

---
+++ source/Entap.Basic.Maui.Chat/Sample/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Entap.Basic.Maui.Core;
+
+namespace Sample
+{
+	public class MainPageViewModel : PageViewModelBase
+	{
+		public MainPageViewModel()
+		{
+		}
+
+		public ProcessCommand ChatCommand => new ProcessCommand(async () =>
+		{
+			await PageManager.Navigation.PushAsync<ChatPage>(new ChatPageViewModel());
+		});
+
+    }
+}
+
---
+++ source/Entap.Basic.Maui.Chat/Sample/UI/Pages/ChatPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Entap.Basic.Maui.Core;
+
+namespace Sample
+{
+	public class ChatPageViewModel : PageViewModelBase
+	{
+		public ChatPageViewModel()
+		{
+		}
+	}
+}
+
