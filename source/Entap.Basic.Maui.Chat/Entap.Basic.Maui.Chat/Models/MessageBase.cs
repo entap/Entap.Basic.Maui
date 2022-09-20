c3a6306bc8548b188828f7bb75feb15b2513ce34
Iga <iga@entap.co.jp>
2022-09-20 13:36:51 +0900

    [add]MessageBase : Entap.Chatから移植

---
+++ source/Entap.Basic.Maui.Chat/Entap.Basic.Maui.Chat/Models/MessageBase.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Maui.Controls.Internals;
+
+namespace Entap.Basic.Maui.Chat
+{
+    [Preserve(AllMembers = true)]
+    public class MessageBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged is not null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public MessageBase(string sendUserId)
+        {
+            SendUserId = sendUserId;
+        }
+
+        public MessageBase(MessageBase notSendMessage)
+        {
+            MessageId = notSendMessage.MessageId;
+            SendDateTime = notSendMessage.SendDateTime;
+            Text = notSendMessage.Text;
+            MediaUrl = notSendMessage.MediaUrl;
+            UserIcon = notSendMessage.UserIcon;
+            MessageType = notSendMessage.MessageType;
+            SendUserId = notSendMessage.SendUserId;
+            AlreadyReadCount = 0;
+            ResendVisible = true;
+        }
+
+        private int messageId;
+        public int MessageId
+        {
+            get
+            {
+                return messageId;
+            }
+            set
+            {
+                if (messageId != value)
+                {
+                    messageId = value;
+                    if (messageId != ChatListView.NotSendMessageId)
+                        ProgressVisible = false;
+                    OnPropertyChanged("MessageId");
+                    OnPropertyChanged("ResendVisible");
+                }
+            }
+        }
+
+        private DateTime sendDateTime;
+        public DateTime SendDateTime
+        {
+            get
+            {
+                return sendDateTime;
+            }
+            set
+            {
+                if (sendDateTime != value)
+                {
+                    sendDateTime = value;
+                    OnPropertyChanged("SendDateTime");
+                }
+            }
+        }
+
+        public string? Text { get; set; }
+
+        private string? mediaUrl;
+        public string? MediaUrl
+        {
+            get
+            {
+                return mediaUrl;
+            }
+            set
+            {
+                if (mediaUrl != value)
+                {
+                    mediaUrl = value;
+                    OnPropertyChanged("MediaUrl");
+                }
+            }
+        }
+
+        private string? mediaThumbnailUrl;
+        public string? MediaThumbnailUrl
+        {
+            get
+            {
+                return mediaThumbnailUrl;
+            }
+            set
+            {
+                if (mediaThumbnailUrl != value)
+                {
+                    mediaThumbnailUrl = value;
+                    OnPropertyChanged("MediaThumbnailUrl");
+                }
+            }
+        }
+
+        private string? userIcon;
+        public string? UserIcon
+        {
+            get
+            {
+                return userIcon;
+            }
+            set
+            {
+                if (userIcon != value)
+                {
+                    userIcon = value;
+                    OnPropertyChanged("UserIcon");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 1:テキスト, 2:画像, 3:動画
+        /// </summary>
+        public int MessageType { get; set; }
+        public string SendUserId { get; set; }
+
+        private int alreadyReadCount;
+        public int AlreadyReadCount
+        {
+            get
+            {
+                return alreadyReadCount;
+            }
+            set
+            {
+                if (alreadyReadCount != value)
+                {
+                    alreadyReadCount = value;
+                    OnPropertyChanged("AlreadyReadCount");
+                }
+            }
+        }
+
+        private bool resendVisible;
+        public bool ResendVisible
+        {
+            get
+            {
+                return resendVisible;
+            }
+            set
+            {
+                if (resendVisible != value)
+                {
+                    resendVisible = value;
+                    OnPropertyChanged("ResendVisible");
+                }
+            }
+        }
+
+        public int NotSendId { get; set; }
+
+        private bool dateVisible;
+        public bool DateVisible
+        {
+            get
+            {
+                return dateVisible;
+            }
+            set
+            {
+                if (dateVisible != value)
+                {
+                    dateVisible = value;
+                    OnPropertyChanged("DateVisible");
+                }
+            }
+        }
+
+        private double uploadProgress;
+        public double UploadProgress
+        {
+            get
+            {
+                return uploadProgress;
+            }
+            set
+            {
+                if (uploadProgress != value)
+                {
+                    uploadProgress = value;
+                    if (uploadProgress >= 1)
+                        ProgressVisible = false;
+                    OnPropertyChanged("UploadProgress");
+                }
+            }
+        }
+        public void HandleUploadProgress(long bytes, long totalBytes)
+        {
+            System.Diagnostics.Debug.WriteLine("Uploading {0}/{1}", bytes, totalBytes);
+            UploadProgress = (double)bytes / (double)totalBytes;
+        }
+        private bool progressVisible;
+        public bool ProgressVisible
+        {
+            get
+            {
+                return progressVisible;
+            }
+            set
+            {
+                if (progressVisible != value)
+                {
+                    progressVisible = value;
+                    if (!progressVisible)
+                        UploadProgress = 0;
+                    OnPropertyChanged("ProgressVisible");
+                }
+            }
+        }
+    }
+}
