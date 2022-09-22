0006dd17c8eca2590de45585e53b4e343d847b8b
Iga <iga@entap.co.jp>
2022-09-22 09:21:22 +0900

    [add]Entap.Basic.Maui.Chat : ChatListViewRenderer(Android)

---
+++ source/Entap.Basic.Maui.Chat/Entap.Basic.Maui.Chat/Platforms/Android/ChatListViewRenderer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Android.Content;
+using Microsoft.Maui.Controls.Handlers.Compatibility;
+using Microsoft.Maui.Controls.Platform;
+
+namespace Entap.Basic.Maui.Chat.Platforms.Android
+{
+	public class ChatListViewRenderer : ListViewRenderer
+    {
+		public ChatListViewRenderer(Context context) : base(context)
+		{
+		}
+        protected override void Dispose(bool disposing)
+        {
+            var _ChatListView = Element as ChatListView;
+            _ChatListView?.Dispose();
+            base.Dispose(disposing);
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null && e.NewElement != null)
+            {
+                Control.Scroll += Control_Scroll;
+            }
+        }
+
+        private void Control_Scroll(object? sender, global::Android.Widget.AbsListView.ScrollEventArgs e)
+        {
+            if (Control is null) return;
+
+            var _ChatListView = Element as ChatListView;
+            if (_ChatListView?.Messages is null || _ChatListView.Messages.Count < 1)
+                return;
+
+            var firstIndex = e.FirstVisibleItem;
+
+            if (_ChatListView.Messages.Count >= firstIndex)
+            {
+                int firstVisibleIndex;
+                object firstVisibleItem;
+                int lastVisibleIndex = 0;
+                if (firstIndex == 0)
+                {
+                    firstVisibleIndex = firstIndex;
+                    firstVisibleItem = _ChatListView.Messages[firstIndex];
+                }
+                else
+                {
+                    firstVisibleIndex = firstIndex - 1;
+                    firstVisibleItem = _ChatListView.Messages[firstIndex - 1];
+                }
+
+                var lastVisibleItem = firstVisibleItem;
+                var lastIndex = firstIndex - 1 + e.VisibleItemCount - 1;
+                for (var i = lastIndex; i >= 0; i--)
+                {
+                    if (_ChatListView.Messages.Count - 1 >= i)
+                    {
+                        lastVisibleIndex = i;
+                        lastVisibleItem = _ChatListView.Messages[i];
+                        break;
+                    }
+                }
+
+                _ChatListView.VisibleItemUpdateForAndroid(firstVisibleIndex, firstVisibleItem, lastVisibleIndex, lastVisibleItem);
+            }
+            var controlY = Control.GetChildAt(0)?.GetY() ?? 0;
+            _ChatListView.OnScrolled(null, new ScrolledEventArgs(0, controlY * -1));
+        }
+    }
+}
--- source/Entap.Basic.Maui.Chat/Sample/MauiProgram.cs
+++ source/Entap.Basic.Maui.Chat/Sample/MauiProgram.cs
@@ -14,13 +14,15 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			})
-            .UseMauiCompatibility()
 			.ConfigureMauiHandlers((handlers) =>
 			{
 #if IOS
                 handlers.AddHandler(typeof(Entap.Basic.Maui.Chat.ChatListView), typeof(Entap.Basic.Maui.Chat.Platforms.iOS.ChatListViewRenderer));
 #endif
-			});
+#if ANDROID
+				handlers.AddHandler(typeof(Entap.Basic.Maui.Chat.ChatListView), typeof(Entap.Basic.Maui.Chat.Platforms.Android.ChatListViewRenderer));
+#endif
+            });
 
 		return builder.Build();
 	}
