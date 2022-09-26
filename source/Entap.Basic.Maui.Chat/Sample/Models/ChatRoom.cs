477b6f77d10d77c082c841a2b50ab62570419364
Iga <iga@entap.co.jp>
2022-09-27 08:13:08 +0900

    [update]Entap.Basic.Maui.Chat : Sample : ChatRoomにIsGroupプロパティ追加

--- source/Entap.Basic.Maui.Chat/Sample/Models/ChatRoom.cs
+++ source/Entap.Basic.Maui.Chat/Sample/Models/ChatRoom.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+
 namespace Sample
 {
 	public class ChatRoom
 	{
-		public ChatRoom(int roomId)
-		{
+		public ChatRoom(int roomId, bool isGroup = false)
+        {
 			Id = roomId;
-		}
+            IsGroup = isGroup;
+        }
 
 		public int Id { get; private set; }
 
-		public int LastReadMessageId { get; set; }
+        public bool IsGroup { get; private set; }
+
+        public int LastReadMessageId { get; set; }
 
     }
 }
