4980f80b80b4c691c60f7abf8c1395d2b9e7a788
Iga <iga@entap.co.jp>
2022-09-26 16:16:27 +0900

    [add]Entap.Basic.Maui.Chat : MessageDataTemplateSelector

---
+++ source/Entap.Basic.Maui.Chat/Entap.Basic.Maui.Chat/Controls/MessageDataTemplateSelector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Entap.Basic.Maui.Chat
+{
+    [Preserve(AllMembers = true)]
+    public class MessageDataTemplateSelector : DataTemplateSelector
+    {
+        // DataTemplates
+        public DataTemplate? OthersTextMessageTemplate { get; set; }
+        public DataTemplate? MyTextMessageTemplate { get; set; }
+        public DataTemplate? MyImageMessageTemplate { get; set; }
+        public DataTemplate? OthersImageMessageTemplate { get; set; }
+        public DataTemplate? MyVideoMessageTemplate { get; set; }
+        public DataTemplate? MemberInAndOutMessageTemplate { get; set; }
+        public DataTemplate? CustomMessageTemplate { get; set; }
+        public DataTemplate? OthersVideoMessageTemplate { get; set; }
+
+        public MessageDataTemplateSelector()
+        {
+        }
+
+        protected override DataTemplate? OnSelectTemplate(object item, BindableObject container)
+        {
+            if (!(item is MessageBase message)) throw new TypeAccessException();
+
+            switch (message.MessageType)
+            {
+                case (int)MessageType.Text:
+                    if (message.SendUserId == Settings.Current.ChatService.GetUserId())
+                        return MyTextMessageTemplate;
+                    else
+                        return OthersTextMessageTemplate;
+
+                case (int)MessageType.Image:
+                    if (message.SendUserId == Settings.Current.ChatService.GetUserId())
+                        return MyImageMessageTemplate;
+                    else
+                        return OthersImageMessageTemplate;
+
+                case (int)MessageType.Video:
+                    if (message.SendUserId == Settings.Current.ChatService.GetUserId())
+                        return MyVideoMessageTemplate;
+                    else
+                        return OthersVideoMessageTemplate;
+
+                case (int)MessageType.MemberAddRoom:
+                    return MemberInAndOutMessageTemplate;
+
+                case (int)MessageType.MemberLeaveRoom:
+                    return MemberInAndOutMessageTemplate;
+
+                case (int)MessageType.Custom:
+                    return CustomMessageTemplate;
+
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
