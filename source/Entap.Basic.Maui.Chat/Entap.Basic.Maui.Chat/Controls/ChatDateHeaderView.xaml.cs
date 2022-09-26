9301e3e92d8b44b814812b37dfb5f272af9d829b
Iga <iga@entap.co.jp>
2022-09-26 16:15:17 +0900

    [add]Entap.Basic.Maui.Chat : ChatDateHeaderView

---
+++ source/Entap.Basic.Maui.Chat/Entap.Basic.Maui.Chat/Controls/ChatDateHeaderView.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace Entap.Basic.Maui.Chat;
+
+public partial class ChatDateHeaderView : ContentView
+{
+	public ChatDateHeaderView()
+	{
+		InitializeComponent();
+	}
+
+    //#region DateVisible BindableProperty
+    //public static readonly BindableProperty DateVisibleProperty = BindableProperty.Create(
+    //    nameof(DateVisible),
+    //    typeof(bool),
+    //    typeof(MessageTemplateBase),
+    //    false,
+    //    defaultBindingMode: BindingMode.Default);
+
+    //public bool DateVisible
+    //{
+    //    get { return (bool)GetValue(DateVisibleProperty); }
+    //    set { SetValue(DateVisibleProperty, value); }
+    //}
+    //#endregion
+
+    //#region SendDateTime BindableProperty
+    //public static readonly BindableProperty SendDateTimeProperty = BindableProperty.Create(
+    //    nameof(SendDateTime),
+    //    typeof(DateTime),
+    //    typeof(MessageTemplateBase),
+    //    DateTime.MinValue,
+    //    defaultBindingMode: BindingMode.Default);
+
+    //public DateTime SendDateTime
+    //{
+    //    get { return (DateTime)GetValue(SendDateTimeProperty); }
+    //    set { SetValue(SendDateTimeProperty, value); }
+    //}
+    //#endregion
+}
