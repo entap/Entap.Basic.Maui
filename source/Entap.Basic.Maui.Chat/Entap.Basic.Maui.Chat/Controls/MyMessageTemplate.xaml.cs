34c6fcb360d25098b029c2f2fb03e04360b50073
Iga <iga@entap.co.jp>
2022-09-28 10:15:32 +0900

    [update]Entap.Basic.Maui.Chat.MyMessageTemplate : バインダブルプロパティ追加（IsGroupChat）

--- source/Entap.Basic.Maui.Chat/Entap.Basic.Maui.Chat/Controls/MyMessageTemplate.xaml.cs
+++ source/Entap.Basic.Maui.Chat/Entap.Basic.Maui.Chat/Controls/MyMessageTemplate.xaml.cs
@@ -7,6 +7,21 @@ public partial class MyMessageTemplate : ContentView
 		InitializeComponent();
 	}
 
+    #region IsGroupChat BindableProperty
+    public static readonly BindableProperty IsGroupChatProperty = BindableProperty.Create(
+        nameof(IsGroupChat),
+        typeof(bool),
+        typeof(MyMessageTemplate),
+        false,
+        defaultBindingMode: BindingMode.Default);
+
+    public bool IsGroupChat
+    {
+        get { return (bool)GetValue(IsGroupChatProperty); }
+        set { SetValue(IsGroupChatProperty, value); }
+    }
+    #endregion
+
     #region Message BindableProperty
     public static readonly BindableProperty MessageProperty = BindableProperty.Create(
         nameof(Message),
