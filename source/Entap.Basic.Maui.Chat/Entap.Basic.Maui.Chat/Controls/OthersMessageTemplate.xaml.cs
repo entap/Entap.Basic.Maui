707e1d2eced5839a38fe7c78a8bb6f2ac0e34843
Iga <iga@entap.co.jp>
2022-09-28 10:16:08 +0900

    [add]Entap.Basic.Maui.Chat : OthersMessageTemplate

---
+++ source/Entap.Basic.Maui.Chat/Entap.Basic.Maui.Chat/Controls/OthersMessageTemplate.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Entap.Basic.Maui.Chat;
+
+public partial class OthersMessageTemplate : ContentView
+{
+	public OthersMessageTemplate()
+	{
+		InitializeComponent();
+	}
+
+    #region Message BindableProperty
+    public static readonly BindableProperty MessageProperty = BindableProperty.Create(
+        nameof(Message),
+        typeof(MessageBase),
+        typeof(OthersMessageTemplate),
+        default(MessageBase),
+        defaultBindingMode: BindingMode.Default);
+
+    public MessageBase Message
+    {
+        get { return (MessageBase)GetValue(MessageProperty); }
+        set { SetValue(MessageProperty, value); }
+    }
+    #endregion
+}
