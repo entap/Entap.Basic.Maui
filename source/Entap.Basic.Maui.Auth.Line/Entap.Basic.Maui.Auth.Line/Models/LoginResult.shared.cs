c51facb300453893a2db741e10b08fed42c9d62d
Iga <iga@entap.co.jp>
2022-10-04 14:37:33 +0900

    [update]LoginResult : nullable ディレクティブの削除（プロジェクト単位でnullable許容済み）

--- source/Entap.Basic.Maui.Auth.Line/Entap.Basic.Maui.Auth.Line/Models/LoginResult.shared.cs
+++ source/Entap.Basic.Maui.Auth.Line/Entap.Basic.Maui.Auth.Line/Models/LoginResult.shared.cs
@@ -9,18 +9,12 @@ namespace Entap.Basic.Maui.Auth.Line
 
         public bool IsCanceled { get; internal set; }
 
-#nullable enable
         public Exception? Exception { get; internal set; }
-#nullable disable
 
         public bool IsFaulted => Exception is not null;
 
-#nullable enable
         public LineAccessTokenResponse? LineAccessToken { get; internal set; }
-#nullable disable
 
-#nullable enable
         public UserProfile? UserProfile { get; internal set; }
-#nullable disable
     }
 }
