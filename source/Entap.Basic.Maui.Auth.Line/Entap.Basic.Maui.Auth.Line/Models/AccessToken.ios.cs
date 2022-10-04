e0e8af9198d412be0fa3cd4f779db7574dec1992
Iga <iga@entap.co.jp>
2022-10-04 15:29:47 +0900

    [update]AccessToken : nullable対応

--- source/Entap.Basic.Maui.Auth.Line/Entap.Basic.Maui.Auth.Line/Models/AccessToken.ios.cs
+++ source/Entap.Basic.Maui.Auth.Line/Entap.Basic.Maui.Auth.Line/Models/AccessToken.ios.cs
@@ -5,20 +5,25 @@ namespace Entap.Basic.Maui.Auth.Line
 {
     public partial class AccessToken
     {
+        public AccessToken(string accesstoken)
+        {
+            AccessTokenAccessToken = accesstoken;
+        }
+
         [JsonProperty("token_type")]
-        public string TokenType { get; set; }
+        public string? TokenType { get; set; }
 
         [JsonProperty("scope")]
-        public string Scope { get; set; }
+        public string? Scope { get; set; }
 
         [JsonProperty("refresh_token")]
-        public string RefreshToken { get; set; }
+        public string? RefreshToken { get; set; }
 
         [JsonProperty("createdAt")]
         public double CreatedAt { get; set; }
 
         [JsonProperty("id_token")]
-        public string IdToken { get; set; }
+        public string? IdToken { get; set; }
 
         [JsonProperty("access_token")]
         public string AccessTokenAccessToken { get; set; }
