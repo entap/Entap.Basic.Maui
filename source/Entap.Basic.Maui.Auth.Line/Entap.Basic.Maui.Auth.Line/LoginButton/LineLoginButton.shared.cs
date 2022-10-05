17d90795727bb0135299fa2a1d83fb146ad23534
Iga <iga@entap.co.jp>
2022-10-05 13:43:44 +0900

    [update]LineLoginButton : Namespace変更

--- source/Entap.Basic.Maui.Auth.Line/Entap.Basic.Maui.Auth.Line/LoginButton/LineLoginButton.shared.cs
+++ source/Entap.Basic.Maui.Auth.Line/Entap.Basic.Maui.Auth.Line/LoginButton/LineLoginButton.shared.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Windows.Input;
 using Xamarin.Forms;
 
-namespace Entap.Basic.Auth.Line
+namespace Entap.Basic.Maui.Auth.Line
 {
     public class LineLoginButton : View
     {
