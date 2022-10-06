613fbf5ec4cb385fe6abaea5fcb462cf730ba447
Iga <iga@entap.co.jp>
2022-10-06 11:36:59 +0900

    [update]Sample : LINE認証初期化処理追加

--- source/Entap.Basic.Maui.Auth.Line/Sample/App.xaml.cs
+++ source/Entap.Basic.Maui.Auth.Line/Sample/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿namespace Sample;
+﻿using Entap.Basic.Maui.Core;
 
 public partial class App : Application
 {
@@ -8,5 +9,6 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+		Entap.Basic.Maui.Auth.Line.LineAuthService.Init("1655277852");
 }
 
