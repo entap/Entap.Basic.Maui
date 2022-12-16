8c24d9f40e6e9d55de4e64c4df6b221e396a3cea
Iga <iga@entap.co.jp>
2022-12-16 11:49:16 +0900

    [add]NullableDatePickerBehavior : DatePickerのNullを許容（iOS, Android）

---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Shared/Behaviors/NullableDatePickerBehavior/NullableDatePickerBehavior.android.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Maui.Platform;
+using AView = global::Android.Views.View;
+
+namespace Entap.Basic.Maui.Core
+{
+	public partial class NullableDatePickerBehavior
+	{
+        DatePicker? _datePicker;
+        MauiDatePicker? _mauiDatePicker;
+
+        protected override void OnAttachedTo(DatePicker bindable, AView platformView)
+        {
+            base.OnAttachedTo(bindable, platformView);
+
+            if (platformView is not MauiDatePicker mauiDatePicker)
+            {
+                return;
+            }
+            _datePicker = bindable;
+            _mauiDatePicker = mauiDatePicker;
+
+            if (bindable.BindingContext != null)
+            {
+                BindingContext = bindable.BindingContext;
+            }
+            bindable.DateSelected += OnDateSelected;
+
+            ClearText();
+        }
+
+        protected override void OnDetachedFrom(DatePicker bindable, AView platformView)
+        {
+            bindable.DateSelected -= OnDateSelected;
+            base.OnDetachedFrom(bindable, platformView);
+        }
+
+        private void OnBindingContextChanged(object? sender, EventArgs e)
+        {
+            OnBindingContextChanged();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            BindingContext = _datePicker?.BindingContext;
+        }
+
+        private void OnDateSelected(object? sender, DateChangedEventArgs e)
+        {
+            NullableDate = _datePicker?.Date;
+        }
+
+        private void ClearText()
+        {
+            if (_mauiDatePicker is null) { return; }
+            _mauiDatePicker.Text = null;
+        }
+    }
+}
+
---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Shared/Behaviors/NullableDatePickerBehavior/NullableDatePickerBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Entap.Basic.Maui.Core
+{
+	public partial class NullableDatePickerBehavior : PlatformBehavior<DatePicker>
+	{
+        #region NullableDate BindableProperty
+        public static readonly BindableProperty NullableDateProperty = BindableProperty.Create(
+            nameof(NullableDate),
+            typeof(DateTime?),
+            typeof(NullableDatePickerBehavior),
+            null,
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                if (bindable is NullableDatePickerBehavior behavior)
+                {
+#if IOS || ANDROID
+                    if (newValue is null)
+                        behavior.ClearText();
+#endif
+                }
+            });
+
+        public DateTime? NullableDate
+        {
+            get { return (DateTime?)GetValue(NullableDateProperty); }
+            set { SetValue(NullableDateProperty, value); }
+        }
+        #endregion
+    }
+}
+
