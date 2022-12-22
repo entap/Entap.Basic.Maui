8929967b98abf806815a7e920aefb54343704c16
Iga <iga@entap.co.jp>
2022-12-23 08:29:47 +0900

    [add]DialogItem, DialogItemStyle

---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Shared/Dialog/DialogItemStyle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Entap.Basic.Maui.Core
+{
+    /// <summary>
+    /// ダイアログアイテムスタイル
+    /// </summary>
+	public enum DialogItemStyle
+	{
+        /// <summary>
+        /// デフォルト
+        /// </summary>
+        Default,
+        /// <summary>
+        /// キャンセル
+        /// </summary>
+        Cancel,
+        /// <summary>
+        /// 破壊的
+        /// </summary>
+        Destructive
+    }
+}
+
