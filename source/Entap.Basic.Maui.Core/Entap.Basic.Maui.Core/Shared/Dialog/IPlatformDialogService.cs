f889df8f7c4c6188e14757143a38a698b873586c
Iga <iga@entap.co.jp>
2022-12-23 10:44:47 +0900

    [add]IPlatformDialogService

---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Shared/Dialog/IPlatformDialogService.cs
@@ -0,0 +1,7 @@
+﻿namespace Entap.Basic.Maui.Core
+{
+    public interface IPlatformDialogService
+    {
+        Task<bool> PlatformDisplayAlertAsync(string title, string message, DialogItem? acceptItem, DialogItem cancelItem);
+    }
+}
