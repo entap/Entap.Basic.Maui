51ff4faac8c6588ed9daab4461cf30e2fc19ac99
Iga <iga@entap.co.jp>
2022-12-23 09:58:39 +0900

    [add]DialogStyle

---
+++ source/Entap.Basic.Maui.Core/Entap.Basic.Maui.Core/Shared/Dialog/DialogStyle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Entap.Basic.Maui.Core
+{
+    /// <summary>
+    /// ダイアログのスタイル
+    /// </summary>
+	public class DialogStyle
+	{
+        /// <summary>
+        /// DialogItemStyle.Default 時の文字色
+        /// </summary>
+        public Color? DefaultTextColor { get; set; }
+
+        /// <summary>
+        /// DialogItemStyle.Cancel 時の文字色
+        /// </summary>
+        public Color? CancelTextColor { get; set; }
+
+        /// <summary>
+        /// DialogItemStyle.Destructive 時の文字色
+        /// </summary>
+        public Color? DestructiveTextColor { get; set; }
+    }
+}
+
